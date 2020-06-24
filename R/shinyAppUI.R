6982cdb80c1d830e67e7d2f9f52e9e656638d1d1
ed <58505594@users.noreply.github.com>
2020-06-24 12:41:34 +0400

    update
    update

--- R/shinyAppUI.R
+++ R/shinyAppUI.R
@@ -2,30 +2,10 @@
 #'
 #' @importFrom graphics hist
 #' @import shiny
-timeoutSeconds <- 900
-
-inactivity <- sprintf("function idleTimer() {
-var t = setTimeout(logout, %s);
-window.onmousemove = resetTimer; // catches mouse movements
-window.onmousedown = resetTimer; // catches mouse movements
-window.onclick = resetTimer;     // catches mouse clicks
-window.onscroll = resetTimer;    // catches scrolling
-window.onkeypress = resetTimer;  //catches keyboard actions
-
-function logout() {
-Shiny.setInputValue('timeOut', '%ss')
-}
-
-function resetTimer() {
-clearTimeout(t);
-t = setTimeout(logout, %s);  // time is in milliseconds (1000 is 1 second)
-}
-}
-idleTimer();", timeoutSeconds*1000, timeoutSeconds, timeoutSeconds*1000)
 
 # create the shiny application user interface
 shinyAppUI <- fluidPage(
-  tags$script(inactivity),
+  #tags$script(inactivity),
   titlePanel("CIMS Transcript Re-Calculator by IT Department"),
   sidebarLayout(
     sidebarPanel(
